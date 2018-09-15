b9caa9eb363ede29405bb9a77c230e733fa1050a
przemour@gmail.com <przemour@gmail.com>
2018-09-15 14:59:08 +0200

    zadanie 35 wykonane

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var proba = n;
+
+function sprawdz () {
+    if (n >= "100" && n <= 200) {
+    console.log('Liczba znajduje się w przedziale');
+    } else {
+    console.log('Liczba nie znajduje się w przedziale');
+    }
+}
